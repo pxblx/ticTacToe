210332fbb92eb7a475db23b6ab682b95449f4552
pxblx <pablomurilloavila@gmail.com>
2020-04-30 14:09:37 +0200

    Comprobar jugada ganadora

--- js/main.js
+++ js/main.js
@@ -7,24 +7,30 @@
 addEventListener("DOMContentLoaded", function () {
 
     // Elementos del tablero
-    let tablero = [[document.getElementById("y0x0"), document.getElementById("y0x1"), document.getElementById("y0x2")],
-    [document.getElementById("y1x0"), document.getElementById("y1x1"), document.getElementById("y1x2")],
-    [document.getElementById("y2x0"), document.getElementById("y2x1"), document.getElementById("y2x2")]];
+    let tablero = [document.getElementById("y0x0"), document.getElementById("y0x1"), 
+    document.getElementById("y0x2"), document.getElementById("y1x0"), document.getElementById("y1x1"), 
+    document.getElementById("y1x2"), document.getElementById("y2x0"), document.getElementById("y2x1"), 
+    document.getElementById("y2x2")];
 
     let info = document.getElementById("info"); // Espacio donde se muestra el turno actual o la jugada ganadora
     let infoP = info.getElementsByTagName("p")[0]; // Texto de información
     let infoImg = info.getElementsByTagName("img")[0]; // Imagen de información
 
     // Listeners para el tablero
-    for (let i = 0; i <= 2; i++) {
-        for (let j = 0; j <= 2; j++) {
-            tablero[i][j].addEventListener("click", function () {
-                this.className = ticTacToe.getTurno();
-                ticTacToe.clickFicha(i, j);
+    for (let i = 0; i < 9; i++) {
+        tablero[i].addEventListener("click", function () {
+            let turno = ticTacToe.getTurno();
+            this.className = turno;
+            let resultado = ticTacToe.clickFicha(i);
+            if (resultado !== false) {
+                infoP.innerHTML = "Han ganado las";
+                infoImg.src = "img/" + turno + ".png";
+            } else {
                 infoImg.src = "img/" + ticTacToe.getTurno() + ".png";
-                this.parentNode.replaceChild(this.cloneNode(true), this);
-            });
-        }
+            }
+            this.parentNode.replaceChild(this.cloneNode(true), this);
+        });
+        
     }
 
     // Reiniciar el juego
--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -5,22 +5,54 @@
  */
 
 let ticTacToe = (function () {
-    let tablero = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]; // Tablero del juego
+    let tablero = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // Tablero del juego
     let turno = "X"; // Turno acual
 
     /**
      * Click en una ficha del tablero
      * 
-     * @param {Number} y Coordenada y del tablero
-     * @param {Number} x Coordenada x del tablero
+     * @param {Number} f Ficha
      * 
      * @returns {Boolean} true si es jugada ganadora o false si no lo es
      */
-    let clickFicha = function (y, x) {
-        tablero[y][x] = turno === "X" ? 2 : 3; // Colocar ficha (2 para las X y 3 para las O)
+    let clickFicha = function (f) {
+        tablero[f] = turno === "X" ? 2 : 3; // Colocar ficha (2 para las X y 3 para las O)
         turno = turno === "X" ? "O" : "X"; // Cambio de turno
 
-        // Jugada ganadora (por hacer...)
+        // Comprobar si es jugada ganadora
+        
+        // Horizontal 1
+        if (tablero[0] * tablero[1] * tablero[2] === 8) return "X";
+        else if (tablero[0] * tablero[1] * tablero[2] === 27) return "O";
+        
+        // Horizontal 2
+        if (tablero[3] * tablero[4] * tablero[5] === 8) return "X";
+        else if (tablero[3] * tablero[4] * tablero[5] === 27) return "O";
+
+        // Horizontal 3
+        if (tablero[6] * tablero[7] * tablero[8] === 8) return "X";
+        else if (tablero[6] * tablero[7] * tablero[8] === 27) return "O";
+
+        // Vertical 1
+        if (tablero[0] * tablero[3] * tablero[6] === 8) return "X";
+        else if (tablero[0] * tablero[3] * tablero[6] === 27) return "O";
+
+        // Vertical 2
+        if (tablero[1] * tablero[4] * tablero[7] === 8) return "X";
+        else if (tablero[1] * tablero[4] * tablero[7] === 27) return "O";
+
+        // Vertical 3
+        if (tablero[2] * tablero[5] * tablero[8] === 8) return "X";
+        else if (tablero[2] * tablero[5] * tablero[8] === 27) return "O";
+
+        // Diagonal 1
+        if (tablero[0] * tablero[4] * tablero[8] === 8) return "X";
+        else if (tablero[0] * tablero[4] * tablero[8] === 27) return "O";
+
+        // Diagonal 2
+        if (tablero[2] * tablero[4] * tablero[6] === 8) return "X";
+        else if (tablero[2] * tablero[4] * tablero[6] === 27) return "O";
+
         return false;
     }
 
